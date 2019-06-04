fbe7ccb134d24410b7aab0529fb3d231b1a44bcb
Simon Maher <simon.maher@integrify.io>
2019-06-04 12:10:44 +0300

    Created CoinComponent, set default props

---
+++ src/CoinContainer.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react"
+
+class CoinContainer extends Component {
+    static defaultProps = {
+        // *Coins is an Array that contains these objects
+        coins: [
+
+            { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+            { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
+        ]
+    }
+    constructor(props) {
+        super(props)
+        this.state = {
+            //    * when the page loads, no coin is displayed
+            currentCoin: null,
+            numFlips: 0,
+            numHeads: 0,
+            numTails: 0
+        }
+    }
+    render() {
+        return (
+            <div className="CoinContainer">
+                <h2>Let's Flip A Coin!!!</h2>
+                <p>Out of {this.state.numFlips} flips, there have been {this.state.numHeads} heads and {this.state.numTails} tails</p>
+
+            </div>
+        )
+    }
+}
+
+export default CoinContainer
